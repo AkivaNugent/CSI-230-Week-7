4ce44a4d20996dbdce78a6f862af99de70acaa4d
AkivaNugent <AkivaNugent@gmail.com>
2024-10-14 09:13:43 -0400

    checked against local computer monitor

--- Scheduled Email Reports of At Risk Users (In Class work)/scheduler.ps1
+++ Scheduled Email Reports of At Risk Users (In Class work)/scheduler.ps1
@@ -1,37 +1,35 @@
-﻿function ChoseTimeToRun($Time){
-    $scheduledTime = Get-ScheduledTask | Where-Object { $_.TaskName -ilike "myTask"}
+﻿function ChoseTimeToRun($Time) {
+    $scheduledTime = Get-ScheduledTask | Where-Object { $_.TaskName -ilike "myTask" }
 
-    if($scheduledTime -ne $null){
+    if ($scheduledTime -ne $null) {
         Write-Host "The task already exists" | Out-String
         DisableAutoRun
     }
-       
+
     Write-Host "Creating new task." | Out-String
 
     $action = New-ScheduledTaskAction -Execute "powershell.exe" `
-              -argument "-File `"C:\CSI230\CSI-230-Week-7\Scheduled Email Reports of At Risk Users (In Class work)\main.ps1`""
+              -Argument "-File `"C:\CSI230\CSI-230-Week-7\Scheduled Email Reports of At Risk Users (In Class work)\main.ps1`""
 
     $trigger = New-ScheduledTaskTrigger -Daily -At $Time
     $principal = New-ScheduledTaskPrincipal -UserId 'champuser' -RunLevel Highest
-    $settings = New-ScheduledTaskSettingsSet -RunOnlyIfNetworkAvailible -WakeToRun
+    $settings = New-ScheduledTaskSettingsSet -RunOnlyIfNetworkAvailable -WakeToRun
     $task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
 
     Register-ScheduledTask 'myTask' -InputObject $task
 
-    Get-ScheduledTask | Where-Object {$_.TaskName -ilike "myTask"}
+    Get-ScheduledTask | Where-Object { $_.TaskName -ilike "myTask" }
 } #End of ChoseTimeToRun()
 
 
-
 function DisableAutoRun() {
-    $scheduledTasks = Get-ScheduledTask | Where-Object {$_.TaskName -ilike "myTask"}
+    $scheduledTasks = Get-ScheduledTask | Where-Object { $_.TaskName -ilike "myTask" }
 
-    if($scheduledTasks -ne $null){
+    if ($scheduledTasks -ne $null) {
         Write-Host "Unregistering the task" | Out-String
         Unregister-ScheduledTask -TaskName 'myTask' -Confirm:$false
     }
-    else{
+    else {
         Write-Host "The task is not registered" | Out-String
     }
-
-} #End of disableAutoRun()
+} #End of DisableAutoRun()
